f8cb1f8d5118fde70c887cc458d8f47929eb1491
Zhili Wen <wenzhili523@gmail.com>
2020-06-04 17:08:34 -0500

    Update the function of ETL .py file

--- data/process_data.py
+++ data/process_data.py
@@ -1,16 +1,68 @@
 import sys
-
+import pandas as pd
+import numpy as np
+from sqlalchemy import create_engine
 
 def load_data(messages_filepath, categories_filepath):
-    pass
+    """
+    Load dataframe from filepath
+
+    Argument:
+        messages_filepath (string) -- path to the messages csv file
+        categories_filepath (string) -- path to the catergories csv file
+
+    Output:
+        df(dataframe) -- Combined dataframe from messages and catergories
+    """
+    messages = pd.read_csv(messages_filepath)
+    categories = pd.read_csv(categories_filepath)
+    df = pd.merge(messages,categories,on='id')
+    return df
 
 
 def clean_data(df):
-    pass
+    """
+    Clean "catergories" column in df
+
+    Argument:
+        df (dataframe) -- Combined dataframe from messages and catergories
+
+    Output:
+        df (dataframe) -- Cleaned dataframe
+    """
+    # split categories into separate category columns
+    categories = df['categories'].str.split(';',expand=True)
+
+    # name the separate category columns with first row of Dataframe
+    row = categories.iloc[0, : ]
+    category_colnames = row.apply(lambda x: x[:-2])
+    categories.columns = category_colnames
+
+    # convert category values to numeric values
+    for column in categories:
+        categories[column] = categories[column].str[-1]
+        categories[column] = categories[column].astype(int)
+
+    # concatenate the original dataframe with the new `categories` dataframe
+    df = df.drop(columns=['categories'],axis=1)
+    df = pd.concat([df, categories], axis=1)
+
+    # drop the duplicates in df
+    df = df.drop_duplicates()
+
+    return df
 
 
 def save_data(df, database_filename):
-    pass  
+    """
+    Save the clean dataframe into an sqlite database
+
+    Argument:
+        df (dataframe) -- Cleaned dataset
+        database_filename (string) -- Sqlite destination file path
+    """
+    engine = create_engine('sqlite:///' + database_filename)
+    df.to_sql('DisasterTable', engine, index=False) 
 
 
 def main():
@@ -24,12 +76,12 @@ def main():
 
         print('Cleaning data...')
         df = clean_data(df)
-        
+
         print('Saving data...\n    DATABASE: {}'.format(database_filepath))
         save_data(df, database_filepath)
-        
+
         print('Cleaned data saved to database!')
-    
+
     else:
         print('Please provide the filepaths of the messages and categories '\
               'datasets as the first and second argument respectively, as '\
